c97d3efc2336fa38b3c57b1b5b07f1e375b2066b
Mohammad Islam <Mohammad.Islam@ons.gov.uk>
2017-07-28 10:57:15 +0100

    merge conflict fix

--- javascript/pull-view.js
+++ javascript/pull-view.js
@@ -99,12 +99,11 @@
           '<span class="username">',this.model.get('user').login,'</span>',
           '<div class="elapsed-time" data-created-at="', this.model.get('created_at'),'">',
             this.secondsToTime(this.model.get('elapsed_time')),
-            '<p class="repo">' + this.model.get('repo') +'</p>',
             '</div>',
           '</div>',
         '<div class="status-holder">', statusString , baseSyncHTML, approvalString, '</div>',
         '</div>',
-
+        '<p class="repo">' + this.model.get('repo') +'</p>',
         '<p><a href="', this.model.get('html_url'), '">',
         ' (#',
         this.model.get('number'),
